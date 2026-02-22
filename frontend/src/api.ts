ba064d86648132d3a1564809068ea2e4106150eb
TarunPatidar04 <tarun.coderfarm@gmail.com>
2026-02-22 10:43:25 +0530

    chore: Update default API base URL to a remote server URL.

--- frontend/src/api.ts
+++ frontend/src/api.ts
@@ -2,7 +2,8 @@ import axios from "axios";
 import Cookies from "js-cookie";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "/api",
+  baseURL:
+    import.meta.env.VITE_API_URL || "https://assignment-fymw.onrender.com/api",
   withCredentials: true,
 });
 
